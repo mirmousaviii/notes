e5b808437940d6f7751deae8dab251970801d1e2
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2016-01-12 17:16:49 +0330

    Use Angular js for show data

---
+++ js/notes-controller.js
@@ -0,0 +1,15 @@
+var app = angular.module('app', []);
+
+app.controller('notesCtrl', function ($scope, $http) {
+
+    $scope.notes = null;
+    $http.get('data/data.json')
+        .success(function (data, status, error, config) {
+            $scope.notes = data;
+        })
+        //TODO: Handle error
+        //.error(function (data, status, error, config) {
+        //    //
+        //});
+
+});
