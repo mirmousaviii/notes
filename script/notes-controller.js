ecb281287b4882564489f4d24fa6ddf52ebee673
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2016-02-22 01:40:43 +0330

    Added tags to page

--- script/notes-controller.js
+++ script/notes-controller.js
@@ -3,9 +3,22 @@ var app = angular.module('app', ['angular-loading-bar', 'angular-scroll-animate'
 app.controller('notesCtrl', function ($scope, $http) {
 
     $scope.notes = null;
+    $scope.tags = null;
     $http.get('data/data.json')
         .success(function (data, status, error, config) {
             $scope.notes = data;
+
+            var tags = [];
+            angular.forEach(data, function(note){
+                angular.forEach(note.tags, function(tag) {
+                    if (tags.indexOf(tag) == -1) {
+                        tags.push(tag);
+                    }
+                });
+            });
+            $scope.tags = tags;
+
+
         })
         //TODO: Handle error
         //.error(function (data, status, error, config) {
