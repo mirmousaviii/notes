868ac1f3ebcacb7c02aee57875f5ab8bc3c185a8
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2016-02-23 01:15:44 +0330

    Back-end for append to data

---
+++ back-end/index.js
@@ -0,0 +1,27 @@
+var prompt = require('prompt');
+var fs = require('fs');
+
+const dataPath = '../data/data.sample.json';
+
+var note = {};
+
+prompt.start();
+
+
+prompt.get(['Subject', 'Description', 'Link', 'Tags'], function (err, result) {
+
+    note.subject = result.Subject;
+    note.description = result.Description;
+    note.link = result.Link;
+    note.tags = result.Tags.split(',');
+
+
+    var notesJson = fs.readFileSync(dataPath);
+    var notesData = JSON.parse(notesJson);
+    notesData.push(note);
+    notesJson = JSON.stringify(notesData, null, 2);
+    fs.writeFile(dataPath, notesJson, function (err) {
+        console.log('\nUpdated data with ' + result.Subject);
+    });
+
+});
